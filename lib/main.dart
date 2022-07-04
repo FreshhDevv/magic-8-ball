545b3992b83d458ceaa8119a39058af26a15dce3
FreshDev <billyhans90@gmail.com>
2022-07-04 14:58:25 +0100

    Step4

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,11 @@ class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset('images/ball1.png'),
+      child: TextButton(
+          onPressed: () {
+            print('I go pressed');
+          },
+          child: Image.asset('images/ball1.png')),
     );
   }
 }
